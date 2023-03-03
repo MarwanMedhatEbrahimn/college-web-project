c6620e97bf1f42639b4e0e622e87a8048689b34c
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2023-03-03 02:25:38 +0200

    add chakra-ui to react project

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Heading, Container, Flex } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container minHeight="100vh" maxW="3xl">
+        <Flex minHeight="100vh" alignItems="center" justifyContent="center" flexDirection="column" >
+          <Heading as="h1" size="3xl">
+            College Web Project
+          </Heading>
+        </Flex>
+      </Container>
+
+
     </div>
   )
 }
