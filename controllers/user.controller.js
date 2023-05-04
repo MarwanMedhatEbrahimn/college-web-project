5f871e0c4d6c2ad27cc8f53e249bd775c8d28f72
MarwanMedhatEbrahimn <117862956+MarwanMedhatEbrahimn@users.noreply.github.com>
2023-05-04 13:49:20 +0300

    add crud functionalities

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,20 +4,41 @@ const prisma = require('../prisma/client')
 
 router.get('/users', async (req, res, next) => {
   try {
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany({include: {
+      Department: true,
+    }})
     res.render('users/index', { users: users, active: req.active })
   } catch (error) {
     next(error)
   }
 })
 
-router.post('/users', async (req, res, next) => {
+router.get('/users/Show_Subject/:id', async (req, res, next) => {
   try {
-    console.log(req.body)
-    res.json('sent')
+    const id = parseInt(req.params.id)
+    const subjects = await prisma.user.findFirst({where: {
+      id: id,
+    },
+    select:{
+      subject:true
+    }
+  })
+  res.render('users/Show_Subjects', { subjects: subjects.subject, active: req.active })
   } catch (error) {
     next(error)
   }
 })
+
+router.get('/users/delete/:id', async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id)
+    const flag = await prisma.User.delete({where:{id:id}}) 
+    res.redirect('/users')
+  } catch (error) {
+    next(error)
+  }
+})
+
+
 
 module.exports = router
