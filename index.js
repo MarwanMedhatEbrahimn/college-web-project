9c4439979c84d11dd949d94e61af595804ddd74a
m0hamedKhalidMG <mk9636736@gmail.com>
2023-05-06 17:54:48 +0300

    bugs are solved

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(bodyParser.json());
 
 const corsOptions = {
   origin: '*'
