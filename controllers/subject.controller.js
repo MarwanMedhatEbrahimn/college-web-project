d8be24d82c024eff17b805789ccce8aef5815d1a
MarwanMedhatEbrahimn <117862956+MarwanMedhatEbrahimn@users.noreply.github.com>
2023-05-25 02:32:54 +0300

    Finish

--- controllers/subject.controller.js
+++ controllers/subject.controller.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const prisma = require('../prisma/client')
+const {isAdmin} = require("../middlewares/auth")
 
 // Subjects page - GET
-router.get('/subjects', async (req, res, next) => {
+router.get('/subjects',isAdmin, async (req, res, next) => {
   try {
     const subjects = await prisma.subject.findMany()
     res.render('subjects/index', { subjects: subjects, active: req.active })
@@ -13,20 +14,29 @@ router.get('/subjects', async (req, res, next) => {
 })
 
 // Insert subject page - GET
-router.get('/subjects/Insert', async (req, res, next) => {
+router.get('/subjects/Insert',isAdmin, async (req, res, next) => {
   try {
-    res.render('subjects/insert_subject', { active: req.active })
+    const Departments = await prisma.Department.findMany({})
+    const subjects = await prisma.subject.findMany({})
+    res.render('subjects/insert_subject', { subjects:subjects,Departments:Departments,active: req.active })
   } catch (error) {
     next(error)
   }
 })
 // Insert subject request - POST
-router.post('/subjects/Insert', async (req, res, next) => {
+router.post('/subjects/Insert',isAdmin, async (req, res, next) => {
   try {
+    var User = JSON.parse(req.body.Use)
+    var dependencies = ""
+    User.forEach(element => {
+      dependencies+=element.name +',' 
+    });
     const subject = await prisma.subject.create({
       data: {
         name: req.body.name,
-        code: req.body.code
+        code: req.body.code,
+        departmentId: Number(req.body.Department),
+        dependencies: dependencies
       }
     })
     res.redirect('/subjects')
@@ -36,37 +46,49 @@ router.post('/subjects/Insert', async (req, res, next) => {
 })
 
 // Update subject page - GET
-router.get('/subjects/Update/:id', async (req, res, next) => {
+router.get('/subjects/Update/:id',isAdmin, async (req, res, next) => {
   try {
     const id = parseInt(req.params.id)
     const subject = await prisma.Subject.findFirst({ where: { id: id } })
+    const Departments = await prisma.Department.findMany({})
+    const subjects = await prisma.subject.findMany({})
+    var aod = subject.dependencies.split(",")
+    const dependencies = subjects.filter((item)=>{
+      for(i=0;i<aod.length;i++){
+        if(aod[i]==item.name){
+          return item
+        }
+      }
+    })
     res.render('subjects/update_subject', {
       subject: subject,
-      active: req.active
+      active: req.active,
+      subjects:subjects,
+      Departments:Departments,
+      dependencies:dependencies
     })
   } catch (error) {
     next(error)
   }
 })
 // Update subject request - POST
-router.post('/subjects/Update/:id', async (req, res, next) => {
+router.post('/subjects/Update/:id',isAdmin, async (req, res, next) => {
   try {
     const id = parseInt(req.params.id)
-    const subject = await prisma.Subject.update({
-      data: { name: req.body.name, code: req.body.code },
-      where: { id: id }
+    var User = JSON.parse(req.body.Use)
+    var dependencies = ""
+    User.forEach(element => {
+      dependencies+=element.name +',' 
+    });
+    const subject = await prisma.subject.update({
+      where: { id: id },
+      data: {
+        name: req.body.name,
+        code: req.body.code,
+        departmentId: Number(req.body.Department),
+        dependencies: dependencies
+      }
     })
-    res.redirect('/subjects')
-  } catch (error) {
-    next(error)
-  }
-})
-
-// Delete subject request - POST
-router.post('/subjects/delete/:id', async (req, res, next) => {
-  try {
-    const id = parseInt(req.params.id)
-    const flag = await prisma.Subject.delete({ where: { id: id } })
     res.redirect('/subjects')
   } catch (error) {
     next(error)
@@ -74,31 +96,17 @@ router.post('/subjects/delete/:id', async (req, res, next) => {
 })
 
 // Subject's students page - GET
-router.get('/subjects/Show_Students/:id', async (req, res, next) => {
+router.get('/subjects/Show_Students/:id',isAdmin, async (req, res, next) => {
   try {
-    const id = parseInt(req.params.id)
-    const Students = await prisma.User.findMany({
+    const id = Number(req.params.id)
+    const Students = await prisma.stateOFsub.findMany({
       where: {
-        subjects: {
-          some: {
-            id: id
-          }
+        subject_id: id
         },
-        isStudent: true
-      }
-    })
-    const department = await prisma.Department.findMany({
-      where: {
-        Subject: {
-          some: {
-            id: id
-          }
-        }
-      }
+        include: {user:{include:{department:true}}}
     })
     res.render('subjects/show_Students', {
       Students: Students,
-      department: department[0],
       active: req.active
     })
   } catch (error) {
@@ -106,27 +114,7 @@ router.get('/subjects/Show_Students/:id', async (req, res, next) => {
   }
 
   // Subject's departments page - GET
-  router.get('/subjects/Show_Departments/:id', async (req, res, next) => {
-    try {
-      const id = parseInt(req.params.id)
-      const Departments = await prisma.Department.findMany({
-          where: {
-            Subject: {
-              some: {
-                id: id
-              }
-            }
-          }
-        }
-      )
-      res.render('subjects/show_Departments', {
-        Departments: Departments,
-        active: req.active
-      })
-    } catch (error) {
-      next(error)
-    }
-  })
+
 })
 
 module.exports = router
